5eb56ac1e158601a2d86050bbdd3b89f00a68a16
ebarberevit <32576834+ebarberevit@users.noreply.github.com>
2018-02-08 10:27:05 -0700

    Add files via upload
    Assignment for in-class Python learning on 2/8

---
+++ Daily Exercise 02082018.py
@@ -0,0 +1,16 @@
+#Your Name - Python Daily Exercise - date of learning
+
+# Instructions - create a file for each of the daily exercises, answer each question seperately
+
+# 1. Write a statement that concatenates at least two variables (you can have more than 2 for this practice).
+
+# 2. Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old. Clue input()
+
+#3 Write a program that prints out the numbers from 1 to 20. 
+# For multiples of three, print "usb" instead of the number and for the mulitiples of five, print "device". 
+# For numbers which are multiples of both three and five, print "usb device".
+# Print a new line after each string or number.
+
+
+
