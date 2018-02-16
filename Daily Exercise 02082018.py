0a35e9d65d0c52734b94d79819b6d45c6659b7f6
ebarberevit <32576834+ebarberevit@users.noreply.github.com>
2018-02-16 09:32:57 -0700

    Update Daily Exercise 02082018.py

--- Daily Exercise 02082018.py
+++ Daily Exercise 02082018.py
@@ -1,9 +1,9 @@
 # Your Name - Python Daily Exercise number (seperate out this activity into three seperate .py files for each exercise) - date of learning
 # For example on line 1 (files should be named like shown on lines 3, 5, 7 for your .py files without the hashtag#):
 # John Smith - Python Daily Exercise 1 - Date you created the file for example month, date, and year like this: 02082018
-#   Remove exercises 2 and 3 for exercise 1
+#   Remove exercise 2 for exercise 1
 # John Smith - Python Daily Exercise 2 - Date you created the file 
-#   Remove exercises 1 and 3 for exercise 2
+#   Remove exercise 1
 
 # Instructions - create a file for each of the daily exercises, answer each question seperately
 
