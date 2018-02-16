9129c9c699ec799eac16d1c6a86bcc7f765f3177
ebarberevit <32576834+ebarberevit@users.noreply.github.com>
2018-02-16 09:05:34 -0700

    Update Daily Exercise 02082018.py

--- Daily Exercise 02082018.py
+++ Daily Exercise 02082018.py
@@ -15,8 +15,3 @@
 
 # 2. Create a program that asks the user to enter their name and their age. 
 # Print out a message addressed to them that tells them the year that they will turn 100 years old. Clue input()
-
-#3 Write a program that prints out the numbers from 1 to 20. 
-# For multiples of three, print "usb" instead of the number and for the mulitiples of five, print "device". 
-# For numbers which are multiples of both three and five, print "usb device".
-# Print a new line after each string or number.
